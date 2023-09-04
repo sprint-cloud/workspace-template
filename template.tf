31c62461ae7a372d0bce94fee38792cf8fd535f3
Klapkop <92648779+Klapkop@users.noreply.github.com>
2023-09-04 12:36:29 +0200

    Set ssh dir perms to 600 on startup
    Set ssh dir perms to 600 on startup

--- template.tf
+++ template.tf
@@ -74,6 +74,9 @@ resource "coder_agent" "main" {
     # Generate DB_URL
     export DB_URL="postgresql://$DB_USER:$DB_PASS@coder-${lower(data.coder_workspace.me.owner)}-db-rw:5432/$DB_USER"
 
+    # Set home perms
+    chmod -R 600 /home/coder/.ssh
+
     # install and start code-server
     curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.8.3
     /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
